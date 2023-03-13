b6a9e2b0ff61e8637f353212e24479800ce8d215
rdjurin <rdjurin@croz.net>
2023-03-13 14:08:40 +0100

    feat: implemented configuration loading for vertx
    Support for load configuration from different sources
    configured in code or by java system properties or environment
    variables.

---
+++ src/main/java/io/github/balconyseats/vertx/config/helper/ConfigurationConstants.java
@@ -0,0 +1,19 @@
+package io.github.balconyseats.vertx.config.helper;
+
+import java.util.List;
+
+public final class ConfigurationConstants {
+
+    public static final List<String> VERTX_APP_CLASSPATH_CONFIG = List.of("application.properties", "application.yml", "application.json");
+    public static final String VERTX_APP_SYSTEM_PROPERTY_NAME = "vertx.configuration";
+    public static final String VERTX_APP_ENV_VARIABLE_NAME = "VERTX_CONFIGURATION";
+    public static final String VERTX_APP_CONFIGURATION_CLASSPATH_DISABLED_SYSTEM_PROPERTY = "vertx.configuration.classpath.disabled";
+    public static final String VERTX_APP_CONFIGURATION_CLASSPATH_DISABLED_ENV_VARIABLE = "VERTX_CONFIGURATION_CLASSPATH_DISABLED";
+
+    public static final char PATH_SEPARATOR = ':';
+
+
+    private ConfigurationConstants() {
+        //helper class
+    }
+}
---
+++ src/main/java/io/github/balconyseats/vertx/config/helper/FeatureType.java
@@ -0,0 +1,6 @@
+package io.github.balconyseats.vertx.config.helper;
+
+public enum FeatureType {
+    DEFAULT_VERTX_STORES,
+    CLASSPATH_CONFIG
+}
